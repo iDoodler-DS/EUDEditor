22e4d020f046faad22d9276ab964d0d4543a8a39
armoha <kein0011@naver.com>
2022-04-27 13:01:44 +0900

    Triggers: WIP location 0-based to 1-based

--- EUD Editor/Module/Trigger.vb
+++ EUD Editor/Module/Trigger.vb
@@ -592,12 +592,7 @@ Public Class Element
                 End If
             Case "Location"
                 If isTocode = True And isnumber = True Then
-                    Try
-                        returnstring -= 1
-                        Return returnstring
-                    Catch ex As Exception
-
-                    End Try
+                    Return returnstring
                 End If
             Case "CText"
                 If isTocode = True Then
